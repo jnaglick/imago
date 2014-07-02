196380e97d76499c381fe67b4768c609efc9fcd2
Boughtmanatee5 <sean@nex9.com>
2014-07-02 12:42:15 -0400

    built

--- dist/coffee.js
+++ dist/coffee.js
@@ -1247,7 +1247,7 @@ Meta = (function() {
       if (!(this[resources[0]] || this[resources[0]].meta[resources[1]].value)) {
         return;
       }
-      if (this[resources[0]].meta[resources[1]].value.type) {
+      if (this[resources[0]].meta[resources[1]].value.type != null) {
         return this[resources[0]].meta[resources[1]].value.value;
       } else {
         return this[resources[0]].meta[resources[1]].value;
