a3a53a990f65719fed566d0609c544218a3c0f21
Sebastian Moreno <sebastian@nex9.com>
2015-02-02 16:34:01 +0100

    calculation service

--- dist/imago-ecommerce.js
+++ dist/imago-ecommerce.js
@@ -1,3 +1,479 @@
+var Calculation,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+Calculation = (function() {
+  Calculation.prototype.cart = void 0;
+
+  Calculation.prototype.stripe = void 0;
+
+  Calculation.prototype.currency = void 0;
+
+  Calculation.prototype.shippingmethods = void 0;
+
+  Calculation.prototype.taxes = void 0;
+
+  Calculation.prototype.currencies = void 0;
+
+  Calculation.prototype.taxincluded = void 0;
+
+  function Calculation($q, $state, $http, $auth, imagoUtils, imagoSettings, orderStorage) {
+    this.$q = $q;
+    this.$state = $state;
+    this.$http = $http;
+    this.$auth = $auth;
+    this.imagoUtils = imagoUtils;
+    this.imagoSettings = imagoSettings;
+    this.orderStorage = orderStorage;
+    this.submit = __bind(this.submit, this);
+    this.calculate = __bind(this.calculate, this);
+    this.calculateTotal = __bind(this.calculateTotal, this);
+    this.getZipTax = __bind(this.getZipTax, this);
+    this.getTaxRate = __bind(this.getTaxRate, this);
+    this.calculateShipping = __bind(this.calculateShipping, this);
+    this.changeShipping = __bind(this.changeShipping, this);
+    this.findShippingRate = __bind(this.findShippingRate, this);
+    this.getShippingRate = __bind(this.getShippingRate, this);
+    this.setShippingRates = __bind(this.setShippingRates, this);
+    this.setCurrency = __bind(this.setCurrency, this);
+    this.applyCoupon = __bind(this.applyCoupon, this);
+    this.checkCoupon = __bind(this.checkCoupon, this);
+    this.changeAddress = __bind(this.changeAddress, this);
+    this.deleteItem = __bind(this.deleteItem, this);
+    this.updateCart = __bind(this.updateCart, this);
+    this.countries = imagoUtils.COUNTRIES;
+  }
+
+  Calculation.prototype.updateCart = function() {
+    this.$http.put(this.imagoSettings.host + '/api/carts/' + this.cart._id, this.cart);
+    return this.calculate();
+  };
+
+  Calculation.prototype.deleteItem = function(item) {
+    var idx;
+    idx = _.findIndex(this.cart.items, {
+      id: item.id
+    });
+    this.cart.items.splice(idx, 1);
+    return this.updateCart();
+  };
+
+  Calculation.prototype.changeAddress = function(section, type) {
+    var _ref, _ref1, _ref2, _ref3;
+    if (((_ref = this.process.form['shipping_address']) != null ? _ref.country : void 0) && type === 'country') {
+      this.setCurrency(null, this.process.form['shipping_address'].country);
+    } else if (type === 'country') {
+      this.setCurrency(null, this.process.form[section].country);
+    }
+    this[section] || (this[section] = {});
+    if ((_ref1 = this.process.form[section].country) === 'United States of America' || _ref1 === 'United States' || _ref1 === 'USA' || _ref1 === 'Canada' || _ref1 === 'Australia') {
+      this[section].disablestates = false;
+      if ((_ref2 = this.process.form[section].country) === 'United States of America' || _ref2 === 'United States') {
+        this[section].states = this.imagoUtils.STATES['USA'];
+      } else {
+        this[section].states = this.imagoUtils.STATES[this.process.form[section].country.toUpperCase()];
+      }
+    } else {
+      this[section].disablestates = true;
+      this[section].states = [];
+    }
+    this.process.form[section].country_code = this.imagoUtils.CODES[this.process.form[section].country];
+    if ((_ref3 = this.process.form['shipping_address']) != null ? _ref3.country : void 0) {
+      this.country = this.process.form['shipping_address'].country;
+      this.state = this.process.form['shipping_address'].state;
+      this.zip = this.process.form['shipping_address'].zip;
+    } else {
+      this.country = this.process.form[section].country;
+      this.state = this.process.form[section].state;
+      this.zip = this.process.form[section].zip;
+    }
+    return this.calculate();
+  };
+
+  Calculation.prototype.checkCoupon = function(code) {
+    if (!code) {
+      this.couponState = '';
+      this.calculate();
+      return;
+    }
+    return this.$http.get(this.imagoSettings.host + '/api/coupons?code=' + code).then((function(_this) {
+      return function(response) {
+        if (response.data.length === 1) {
+          _this.coupon = response.data[0];
+          _this.couponState = 'valid';
+          return _this.calculate();
+        } else {
+          return _this.couponState = 'invalid';
+        }
+      };
+    })(this));
+  };
+
+  Calculation.prototype.applyCoupon = function() {
+    var meta, percentvalue, value;
+    if (!this.coupon) {
+      return;
+    }
+    meta = this.coupon.meta;
+    if (meta.type === 'flat') {
+      value = Math.min(this.costs.subtotal, meta.value[this.currency]);
+      return this.costs.subtotal = this.costs.subtotal - value;
+    } else if (meta.type === 'percent') {
+      percentvalue = Number((this.costs.subtotal * meta.value / 10000).toFixed(0));
+      return this.costs.subtotal = this.costs.subtotal - percentvalue;
+    } else if (meta.type === 'free shipping') {
+      return this.costs.shipping = 0;
+    }
+  };
+
+  Calculation.prototype.setCurrency = function(currency, country) {
+    if (country) {
+      currency = this.imagoUtils.inUsa(country) ? 'USD' : this.imagoUtils.CURRENCY_MAPPING[country];
+    }
+    return this.currency = __indexOf.call(this.currencies, currency) >= 0 ? currency : this.currencies[0];
+  };
+
+  Calculation.prototype.setShippingRates = function(rates) {
+    if (rates != null ? rates.length : void 0) {
+      if (_.isPlainObject(rates)) {
+        this.shippingRates = [rates];
+      } else if (_.isArray(rates)) {
+        this.shippingRates = rates;
+      }
+    } else {
+      this.shippingRates = [];
+    }
+    if (this.shippingRates.length) {
+      return this.shipping_options = this.shippingRates[0];
+    }
+  };
+
+  Calculation.prototype.getShippingRate = function() {
+    var deferred, rates;
+    deferred = this.$q.defer();
+    rates = this.findShippingRate();
+    this.setShippingRates(rates);
+    deferred.resolve(rates);
+    return deferred.promise;
+  };
+
+  Calculation.prototype.findShippingRate = function() {
+    var rates, rates_by_country, _ref;
+    if (!this.country) {
+      return;
+    }
+    if ((_ref = this.country) === 'United States of America' || _ref === 'United States') {
+      this.country = 'USA';
+    }
+    rates_by_country = _.filter(this.shippingmethods, (function(_this) {
+      return function(item) {
+        var c, _ref1, _ref2;
+        return item.active && (_ref1 = (_ref2 = _this.country) != null ? _ref2.toUpperCase() : void 0, __indexOf.call((function() {
+          var _i, _len, _ref3, _results;
+          _ref3 = item.countries;
+          _results = [];
+          for (_i = 0, _len = _ref3.length; _i < _len; _i++) {
+            c = _ref3[_i];
+            _results.push(c.toUpperCase());
+          }
+          return _results;
+        })(), _ref1) >= 0);
+      };
+    })(this));
+    if (this.state) {
+      rates = _.filter(rates_by_country, (function(_this) {
+        return function(item) {
+          var s, _ref1;
+          return _ref1 = _this.state.toUpperCase(), __indexOf.call((function() {
+            var _i, _len, _ref2, _results;
+            _ref2 = item.states;
+            _results = [];
+            for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+              s = _ref2[_i];
+              _results.push(s.toUpperCase());
+            }
+            return _results;
+          })(), _ref1) >= 0;
+        };
+      })(this));
+      if (rates != null ? rates.length : void 0) {
+        return rates;
+      }
+      rates = _.filter(rates_by_country, (function(_this) {
+        return function(item) {
+          return item.states.length === 0;
+        };
+      })(this)) || _.filter(this.shippingmethods, function(item) {
+        return !item.countries.length;
+      });
+      return rates;
+    } else {
+      return rates_by_country || _.filter(this.shippingmethods, function(item) {
+        return !item.countries.length;
+      });
+    }
+  };
+
+  Calculation.prototype.changeShipping = function() {
+    this.calcShipping(this.shipping_options, this.$q.defer());
+    return this.calculateTotal();
+  };
+
+  Calculation.prototype.calculateShipping = function() {
+    var deferred;
+    deferred = this.$q.defer();
+    this.costs.shipping = 0;
+    return this.getShippingRate().then((function(_this) {
+      return function(rates) {
+        if (!(rates != null ? rates.length : void 0)) {
+          return deferred.resolve();
+        }
+        _this.calcShipping(rates[0], deferred);
+        return deferred.promise;
+      };
+    })(this));
+  };
+
+  Calculation.prototype.calcShipping = function(rate, deferred) {
+    var count, item, range, with_shippingcost, _i, _j, _len, _len1, _ref, _ref1, _ref2, _ref3;
+    count = 0;
+    with_shippingcost = [];
+    _ref = this.cart.items;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      item = _ref[_i];
+      if (!((_ref1 = item.shipping_cost) != null ? _ref1[this.currency] : void 0)) {
+        if (rate.type === 'weight') {
+          count += item.weight * item.qty;
+        } else {
+          count += item.qty;
+        }
+      } else if ((_ref2 = item.shipping_cost) != null ? _ref2[this.currency] : void 0) {
+        with_shippingcost.push(item);
+      }
+    }
+    if (count === 0 && rate.type !== 'weight' && !with_shippingcost.length) {
+      return deferred.resolve();
+    }
+    range = _.find(rate.ranges, function(range) {
+      return count <= range.to_unit && count >= range.from_unit;
+    });
+    if (!range) {
+      range = rate.ranges[rate.ranges.length - 1] || 0;
+    }
+    if (rate.type === 'weight') {
+      this.costs.shipping = range.price[this.currency] || 0;
+    } else {
+      this.costs.shipping = (range.price[this.currency] || 0) * count;
+    }
+    for (_j = 0, _len1 = with_shippingcost.length; _j < _len1; _j++) {
+      item = with_shippingcost[_j];
+      this.costs.shipping += (((_ref3 = item.shipping_cost) != null ? _ref3[this.currency] : void 0) || 0) * item.qty;
+    }
+    return deferred.resolve();
+  };
+
+  Calculation.prototype.calculateTax = function() {
+    var deferred;
+    deferred = this.$q.defer();
+    this.getTaxRate().then((function(_this) {
+      return function() {
+        var item, onepercent, _i, _j, _len, _len1, _ref, _ref1, _results;
+        _this.costs.tax = 0;
+        if (_this.taxincluded) {
+          deferred.resolve();
+          return;
+        }
+        if (_this.imagoUtils.includesTax(_this.currency)) {
+          _this.costs.includedTax = 0;
+          if (_this.costs.taxRate) {
+            _ref = _this.cart.items;
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              item = _ref[_i];
+              onepercent = item.fields.price.value[_this.currency] / (100 + _this.costs.taxRate) * item.qty;
+              _this.costs.includedTax += Math.round(onepercent * _this.costs.taxRate);
+              _results.push(deferred.resolve());
+            }
+            return _results;
+          } else {
+            return deferred.resolve();
+          }
+        } else {
+          _ref1 = _this.cart.items;
+          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+            item = _ref1[_j];
+            _this.costs.tax += Math.round(item.fields.price.value[_this.currency] * item.qty * _this.costs.taxRate);
+          }
+          return deferred.resolve();
+        }
+      };
+    })(this));
+    return deferred.promise;
+  };
+
+  Calculation.prototype.getTaxRate = function() {
+    var deferred, tRate;
+    deferred = this.$q.defer();
+    this.costs.taxRate = 0;
+    if (this.taxincluded) {
+      deferred.resolve();
+    }
+    if (!this.country) {
+      deferred.resolve();
+    }
+    tRate = this.findTaxRate();
+    if (tRate.autotax && this.imagoUtils.inUsa(this.country)) {
+      return this.getZipTax();
+    }
+    this.costs.taxRate = tRate.rate / 100;
+    deferred.resolve();
+    return deferred.promise;
+  };
+
+  Calculation.prototype.findTaxRate = function() {
+    var rate, rates, rates_by_country, _ref;
+    if ((_ref = this.country) === 'United States of America' || _ref === 'United States') {
+      this.country = 'USA';
+    }
+    rates_by_country = _.filter(this.taxes, (function(_this) {
+      return function(item) {
+        var c, _ref1, _ref2;
+        return item.active && (_ref1 = (_ref2 = _this.country) != null ? _ref2.toUpperCase() : void 0, __indexOf.call((function() {
+          var _i, _len, _ref3, _results;
+          _ref3 = item.countries;
+          _results = [];
+          for (_i = 0, _len = _ref3.length; _i < _len; _i++) {
+            c = _ref3[_i];
+            _results.push(c.toUpperCase());
+          }
+          return _results;
+        })(), _ref1) >= 0);
+      };
+    })(this));
+    if (this.state) {
+      rate = _.find(rates_by_country, (function(_this) {
+        return function(item) {
+          var s, _ref1;
+          return _ref1 = _this.state.toUpperCase(), __indexOf.call((function() {
+            var _i, _len, _ref2, _results;
+            _ref2 = item.states;
+            _results = [];
+            for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+              s = _ref2[_i];
+              _results.push(s.toUpperCase());
+            }
+            return _results;
+          })(), _ref1) >= 0;
+        };
+      })(this));
+      if (rate) {
+        return rate;
+      }
+      rates = _.filter(rates_by_country, function(item) {
+        return item.states.length === 0;
+      });
+      return (rates != null ? rates[0] : void 0) || {
+        'rate': 0
+      };
+    } else {
+      return (rates_by_country != null ? rates_by_country[0] : void 0) || {
+        'rate': 0
+      };
+    }
+  };
+
+  Calculation.prototype.getZipTax = function() {
+    var deferred, _ref;
+    deferred = this.$q.defer();
+    if (!(this.zip || (((_ref = this.zip) != null ? _ref.length : void 0) > 4))) {
+      deferred.resolve();
+    } else {
+      this.$http.get(("" + this.imagoSettings.host + "/api/ziptax?zipcode=") + this.zip).then((function(_this) {
+        return function(response) {
+          _this.costs.taxRate = response.data.taxUse;
+          return deferred.resolve();
+        };
+      })(this));
+    }
+    return deferred.promise;
+  };
+
+  Calculation.prototype.calculateTotal = function() {
+    this.costs.total = 0;
+    if (this.costs.subtotal) {
+      this.costs.total += this.costs.subtotal;
+    }
+    if (this.costs.shipping) {
+      this.costs.total += this.costs.shipping;
+    }
+    if (this.costs.tax && !this.taxincluded) {
+      this.costs.total += this.costs.tax;
+    }
+    return this.costs.total;
+  };
+
+  Calculation.prototype.calculate = function() {
+    var item, _i, _len, _ref;
+    this.costs = {
+      subtotal: 0,
+      shipping: 0,
+      tax: 0,
+      includedTax: 0,
+      total: 0
+    };
+    _ref = this.cart.items;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      item = _ref[_i];
+      this.costs.subtotal += item.qty * item.fields.price.value[this.currency];
+    }
+    this.costs.total = this.costs.subtotal;
+    this.applyCoupon();
+    return this.$q.all([this.calculateTax(), this.calculateShipping()]).then((function(_this) {
+      return function() {
+        return _this.calculateTotal();
+      };
+    })(this));
+  };
+
+  Calculation.prototype.submit = function() {
+    if (this.processing) {
+      return;
+    }
+    this.processing = true;
+    this.process.form.items = angular.copy(this.cart.items);
+    this.process.form.costs = angular.copy(this.costs);
+    this.process.form.currency = angular.copy(this.currency);
+    this.process.form.billing_address.name = angular.copy(this.process.form.card.name);
+    if (!this.differentshipping) {
+      this.process.form['shipping_address'] = angular.copy(this.process.form['billing_address']);
+    }
+    return this.$http.post(this.imagoSettings.host + '/api/checkout', this.process.form).then((function(_this) {
+      return function(response) {
+        var order, _i, _len, _ref;
+        console.log('response checkout', response);
+        _this.$auth.setToken(response.data.token);
+        if (response.data.code === 200) {
+          _ref = response.data.message;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            order = _ref[_i];
+            console.log('order', order);
+            _this.$state.go('order', {
+              number: order.number
+            });
+            break;
+          }
+        }
+        return _this.processing = false;
+      };
+    })(this));
+  };
+
+  return Calculation;
+
+})();
+
+angular.module('imago').service('calculation', ['$q', '$state', '$http', '$auth', 'imagoUtils', 'imagoSettings', 'orderStorage', Calculation]);
+
 var imagoCart;
 
 imagoCart = (function() {
---
+++ src/imago-ecommerce/calculation.service.coffee
@@ -0,0 +1,280 @@
+class Calculation extends Service
+
+  cart            : undefined
+  stripe          : undefined
+  currency        : undefined
+  shippingmethods : undefined
+  taxes           : undefined
+  currencies      : undefined
+  taxincluded     : undefined
+
+  constructor: (@$q, @$state, @$http, @$auth, @imagoUtils, @imagoSettings, @orderStorage) ->
+    @countries = imagoUtils.COUNTRIES
+
+  updateCart: =>
+    @$http.put(@imagoSettings.host + '/api/carts/' + @cart._id, @cart)
+    @calculate()
+
+  deleteItem: (item) =>
+    idx = _.findIndex @cart.items, {id: item.id}
+    @cart.items.splice idx, 1
+    @updateCart()
+
+  changeAddress: (section, type) =>
+    if @process.form['shipping_address']?.country and type is 'country'
+      @setCurrency(null, @process.form['shipping_address'].country)
+    else if type is 'country'
+      @setCurrency(null, @process.form[section].country)
+    @[section] or= {}
+    if @process.form[section].country in ['United States of America', 'United States', 'USA', 'Canada', 'Australia']
+      @[section].disablestates = false
+      if @process.form[section].country in ['United States of America', 'United States']
+        @[section].states = @imagoUtils.STATES['USA']
+      else
+        @[section].states = @imagoUtils.STATES[@process.form[section].country.toUpperCase()]
+    else
+      @[section].disablestates = true
+      @[section].states = []
+    @process.form[section].country_code = @imagoUtils.CODES[@process.form[section].country]
+
+    if @process.form['shipping_address']?.country
+      @country = @process.form['shipping_address'].country
+      @state   = @process.form['shipping_address'].state
+      @zip     = @process.form['shipping_address'].zip
+    else
+      @country = @process.form[section].country
+      @state   = @process.form[section].state
+      @zip     = @process.form[section].zip
+
+    @calculate()
+
+  checkCoupon: (code) =>
+    unless code
+      @couponState = ''
+      @calculate()
+      return
+
+    @$http.get(@imagoSettings.host + '/api/coupons?code=' + code).then (response) =>
+      if response.data.length is 1
+        @coupon = response.data[0]
+        @couponState = 'valid'
+        @calculate()
+
+      else
+        @couponState = 'invalid'
+
+  applyCoupon: =>
+    return unless @coupon
+    meta = @coupon.meta
+
+    if meta.type is 'flat'
+      value = Math.min(@costs.subtotal, meta.value[@currency])
+      @costs.subtotal = @costs.subtotal - value
+    else if meta.type is 'percent'
+      percentvalue = Number((@costs.subtotal * meta.value / 10000).toFixed(0))
+      @costs.subtotal = @costs.subtotal - percentvalue
+    else if meta.type is 'free shipping'
+      @costs.shipping = 0
+
+  setCurrency: (currency, country) =>
+    if country
+      currency = if @imagoUtils.inUsa(country) then 'USD' else \
+                    @imagoUtils.CURRENCY_MAPPING[country]
+    @currency = if currency in @currencies then currency else @currencies[0]
+
+  setShippingRates: (rates) =>
+    if rates?.length
+      if _.isPlainObject rates
+        @shippingRates = [rates]
+      else if _.isArray rates
+        @shippingRates = rates
+    else
+      @shippingRates = []
+
+    if @shippingRates.length
+      @shipping_options = @shippingRates[0]
+
+  getShippingRate: =>
+    deferred = @$q.defer()
+    rates = @findShippingRate()
+    @setShippingRates(rates)
+    deferred.resolve(rates)
+    return deferred.promise
+
+  findShippingRate: =>
+    return unless @country
+
+    if @country in ['United States of America', 'United States']
+      @country = 'USA'
+
+
+    # get all rates for this country
+    rates_by_country = _.filter @shippingmethods, (item) =>
+      item.active and @country?.toUpperCase() in (c.toUpperCase() for c in item.countries)
+
+    if @state
+      # check if there is a rate specific for this state
+      rates = _.filter rates_by_country, (item) => @state.toUpperCase() in (s.toUpperCase() for s in item.states)
+      return rates if rates?.length
+      # console.log 'rates after first loop...', rates
+      # if we didnt find any rates yet check if there is a less specific rate.
+      rates = _.filter(rates_by_country, (item) => item.states.length is 0) or \
+             _.filter(@shippingmethods, (item) -> !item.countries.length)
+      return rates
+    else
+      return rates_by_country or _.filter @shippingmethods, (item) -> !item.countries.length
+
+  changeShipping: =>
+    @calcShipping(@shipping_options, @$q.defer())
+    @calculateTotal()
+
+  calculateShipping: =>
+    deferred = @$q.defer()
+
+    @costs.shipping = 0
+
+    @getShippingRate().then (rates) =>
+      return deferred.resolve() unless rates?.length
+      @calcShipping(rates[0], deferred)
+      return deferred.promise
+
+  calcShipping: (rate, deferred) ->
+    count = 0
+    with_shippingcost = []
+    for item in @cart.items
+      # continue unless item.shipping
+      if not item.shipping_cost?[@currency]
+        if rate.type is 'weight'
+          count += item.weight * item.qty
+        else
+          count += item.qty
+      else if item.shipping_cost?[@currency]
+        with_shippingcost.push(item)
+
+    if count is 0 and rate.type isnt 'weight' and not with_shippingcost.length
+      # @costs.shipping = 0
+      return deferred.resolve()
+
+    range = _.find rate.ranges, (range) -> count <= range.to_unit and count >= range.from_unit
+    range = rate.ranges[rate.ranges.length - 1] or 0 if not range
+    # console.log 'rage is', range
+    if rate.type is 'weight'
+      @costs.shipping = range.price[@currency] or 0
+    else
+      @costs.shipping = (range.price[@currency] or 0) * count
+
+    for item in with_shippingcost
+      @costs.shipping += (item.shipping_cost?[@currency] or 0) * item.qty
+    return deferred.resolve()
+
+
+  calculateTax: ->
+    deferred = @$q.defer()
+
+    @getTaxRate().then =>
+      @costs.tax = 0
+      if @taxincluded
+        deferred.resolve()
+        return
+      # console.log 'currency', @currency, 'includestax', @includesTax(@currency)
+      if @imagoUtils.includesTax(@currency)
+        @costs.includedTax = 0
+        if @costs.taxRate
+          for item in @cart.items
+            onepercent = item.fields.price.value[@currency]/(100+@costs.taxRate) * item.qty
+            @costs.includedTax += Math.round(onepercent * @costs.taxRate)
+            deferred.resolve()
+        else
+          deferred.resolve()
+      else
+        for item in @cart.items
+          @costs.tax += Math.round(item.fields.price.value[@currency] * item.qty * @costs.taxRate)
+        deferred.resolve()
+    return deferred.promise
+
+  getTaxRate: =>
+    deferred = @$q.defer()
+
+    @costs.taxRate = 0
+    deferred.resolve() if @taxincluded
+    deferred.resolve() if not @country
+
+    tRate = @findTaxRate()
+    # console.log 'tRate', tRate
+    return @getZipTax() if tRate.autotax and @imagoUtils.inUsa(@country)
+    @costs.taxRate = tRate.rate / 100
+
+    deferred.resolve()
+    return deferred.promise
+
+  findTaxRate: ->
+    if @country in ['United States of America', 'United States']
+      @country = 'USA'
+
+    rates_by_country = _.filter(@taxes, (item) => item.active and \
+                                @country?.toUpperCase() in (c.toUpperCase() for c in item.countries))
+    if @state
+      rate = _.find rates_by_country, (item) =>
+          @state.toUpperCase() in (s.toUpperCase() for s in item.states)
+      return rate if rate
+      # if we didnt find any rates yet check if there is a less specific rate.
+      rates = _.filter rates_by_country, (item) -> item.states.length is 0
+      return rates?[0] or {'rate': 0}
+    else
+      return rates_by_country?[0] or {'rate': 0}
+
+  getZipTax: =>
+    deferred = @$q.defer()
+    if not (@zip or (@zip?.length > 4))
+      deferred.resolve()
+    else
+      @$http.get("#{@imagoSettings.host}/api/ziptax?zipcode="+@zip)
+        .then (response) =>
+          @costs.taxRate = response.data.taxUse
+          deferred.resolve()
+    return deferred.promise
+
+  calculateTotal: =>
+    @costs.total = 0
+    @costs.total += @costs.subtotal if @costs.subtotal
+    @costs.total += @costs.shipping if @costs.shipping
+    @costs.total += @costs.tax if @costs.tax and !@taxincluded
+    @costs.total
+
+  calculate: =>
+    @costs =
+      subtotal    : 0
+      shipping    : 0
+      tax         : 0
+      includedTax : 0
+      total       : 0
+
+    for item in @cart.items
+      @costs.subtotal += item.qty * item.fields.price.value[@currency]
+    @costs.total = @costs.subtotal
+
+    @applyCoupon()
+
+    @$q.all([@calculateTax(), @calculateShipping()]).then =>
+      @calculateTotal()
+
+  submit: =>
+    return if @processing
+    @processing = true
+    @process.form.items    = angular.copy @cart.items
+    @process.form.costs    = angular.copy @costs
+    @process.form.currency = angular.copy @currency
+    @process.form.billing_address.name = angular.copy @process.form.card.name
+
+    if not @differentshipping
+      @process.form['shipping_address'] = angular.copy @process.form['billing_address']
+    @$http.post(@imagoSettings.host + '/api/checkout', @process.form).then (response) =>
+      console.log 'response checkout', response
+      @$auth.setToken(response.data.token)
+      if response.data.code is 200
+        for order in response.data.message
+          console.log 'order', order
+          @$state.go('order', {number: order.number})
+          break
+
+      @processing = false
