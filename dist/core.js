1a56d9d1af6c688180754e963dd54cb87ff77e7f
Sebastian Moreno <sebastian@nex9.com>
2015-07-28 10:56:13 -0400

    format currency function

--- dist/core.js
+++ dist/core.js
@@ -1724,6 +1724,20 @@ imagoUtils = (function() {
         ints = value.slice(0, value.length - decimal) || '0';
         return ints + "." + floats;
       },
+      formatCurrency: function(number, c, d, t) {
+        var t;
+        var d;
+        var c;
+        var i, j, n, s;
+        n = number;
+        c = isNaN(c = Math.abs(c)) ? 2 : c;
+        d = d === void 0 ? '.' : d;
+        t = t === void 0 ? ',' : t;
+        s = n < 0 ? '-' : '';
+        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + '';
+        j = (j = i.length) > 3 ? j % 3 : 0;
+        return s + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : '');
+      },
       toPrice: function(value, currency) {
         var price, symbol;
         price = this.toFloat(value).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
--- src/core/imagoUtils.factory.coffee
+++ src/core/imagoUtils.factory.coffee
@@ -915,6 +915,19 @@ class imagoUtils extends Factory
 
         return "#{ints}.#{floats}"
 
+      formatCurrency: (number, c, d, t) ->
+        `var t`
+        `var d`
+        `var c`
+        n = number
+        c = if isNaN(c = Math.abs(c)) then 2 else c
+        d = if d == undefined then '.' else d
+        t = if t == undefined then ',' else t
+        s = if n < 0 then '-' else ''
+        i = parseInt(n = Math.abs(+n or 0).toFixed(c)) + ''
+        j = if (j = i.length) > 3 then j % 3 else 0
+        s + (if j then i.substr(0, j) + t else '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t) + (if c then d + Math.abs(n - i).toFixed(c).slice(2) else '')
+
       toPrice: (value, currency) ->
         price  = @toFloat(value).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
         symbol = @getCurrencySymbol(currency)
