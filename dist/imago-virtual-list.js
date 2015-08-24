1274e78c4379593b25fb8142770428b2e227deda
Sebastian Moreno <sebastian@nex9.com>
2015-08-24 17:23:24 -0400

    imago virtual list v3

--- dist/imago-virtual-list.js
+++ dist/imago-virtual-list.js
@@ -1,46 +1,68 @@
 var ImagoVirtualList;
 
 ImagoVirtualList = (function() {
-  function ImagoVirtualList($timeout) {
+  function ImagoVirtualList($window, $rootScope) {
     return {
-      scope: {
+      scope: true,
+      templateUrl: '/imago/imago-virtual-list.html',
+      transclude: true,
+      controller: function() {},
+      controllerAs: 'imagovirtuallist',
+      bindToController: {
         data: '=',
         rowHeight: '=',
         rowWidth: '='
       },
-      templateUrl: '/imago/imago-virtual-list.html',
-      transclude: true,
       link: function(scope, element, attrs, ctrl, transclude) {
+        var watcher;
         transclude(scope, function(clone, scope) {
           return element.children().append(clone);
         });
-        scope.width = element[0].clientWidth;
         scope.scrollTop = 0;
+        scope.width = element[0].clientWidth;
         scope.visibleProvider = [];
         scope.cellsPerPage = 0;
         scope.numberOfCells = 0;
         scope.canvasHeight = {};
-        scope.init = function() {
-          scope.height = element[0].clientHeight;
-          element[0].addEventListener('scroll', scope.onScroll);
+        scope.init = function(rowWidth, rowHeight) {
+          var testDiv;
+          if (!rowWidth || !rowHeight) {
+            testDiv = document.createElement('div');
+            testDiv.className = attrs.classitem;
+            testDiv.id = 'virtual-list-test-div';
+            element.append(testDiv);
+            rowWidth = rowWidth || testDiv.clientWidth;
+            rowHeight = rowHeight || testDiv.clientHeight;
+            angular.element(element[0].querySelector('#virtual-list-test-div')).remove();
+          }
+          scope.rowWidth = rowWidth;
+          scope.rowHeight = rowHeight;
+          if (!scope.width) {
+            scope.width = element[0].clientWidth;
+          }
+          if (!scope.height) {
+            scope.height = element[0].clientHeight;
+          }
+          if (attrs.imagoVirtualListContainer) {
+            element[0].addEventListener('scroll', scope.onScrollContainer);
+          } else {
+            angular.element($window).on('scroll', scope.onScrollWindow);
+          }
           scope.itemsPerRow = Math.round(scope.width / scope.rowWidth);
+          scope.canvasHeight = {
+            height: Math.ceil((scope.imagovirtuallist.data.length * scope.rowHeight) / scope.itemsPerRow) + 'px'
+          };
           scope.cellsPerPage = Math.round(scope.height / scope.rowHeight) * scope.itemsPerRow;
           scope.numberOfCells = 3 * scope.cellsPerPage;
-          scope.canvasHeight = {
-            height: Math.ceil((scope.data.length * scope.rowHeight) / scope.itemsPerRow) + 'px'
-          };
+          scope.margin = 0;
           return scope.updateDisplayList();
         };
         scope.updateDisplayList = function() {
-          var cellsToCreate, chunks, data, findIndex, firstCell, i, idx, oldVisible, results;
+          var cellsToCreate, chunks, data, findIndex, firstCell, i, idx, results;
           firstCell = Math.max(Math.floor(scope.scrollTop / scope.rowHeight) - (Math.round(scope.height / scope.rowHeight)), 0);
           cellsToCreate = Math.min(firstCell + scope.numberOfCells, scope.numberOfCells);
-          oldVisible = angular.copy(scope.visibleProvider);
           data = firstCell * scope.itemsPerRow;
-          scope.visibleProvider = scope.data.slice(data, data + cellsToCreate);
-          if (angular.equals(scope.visibleProvider, oldVisible)) {
-            return;
-          }
+          scope.visibleProvider = scope.imagovirtuallist.data.slice(data, data + cellsToCreate);
           chunks = _.chunk(scope.visibleProvider, scope.itemsPerRow);
           i = 0;
           results = [];
@@ -64,27 +86,46 @@ ImagoVirtualList = (function() {
             };
             idx = findIndex();
             scope.visibleProvider[i].styles = {
-              'transform': "translate3d(" + ((scope.rowWidth * idx.inside) + 'px') + ", " + ((firstCell + idx.chunk) * scope.rowHeight + 'px') + ", 0)"
+              'transform': "translate3d(" + ((scope.rowWidth * idx.inside) + scope.margin + 'px') + ", " + ((firstCell + idx.chunk) * scope.rowHeight + 'px') + ", 0)"
             };
             results.push(i++);
           }
           return results;
         };
-        scope.onScroll = function() {
+        scope.onScrollContainer = function() {
           scope.scrollTop = element.prop('scrollTop');
           scope.updateDisplayList();
           return scope.$apply();
         };
-        return scope.$watchGroup(['data', 'rowHeight', 'rowWidth'], function() {
-          var rowWidth;
-          if (!scope.data || !scope.rowWidth || !scope.rowHeight) {
+        scope.onScrollWindow = function() {
+          scope.scrollTop = $window.pageYOffset;
+          scope.updateDisplayList();
+          return scope.$apply();
+        };
+        scope.$watchGroup(['imagovirtuallist.data', 'imagovirtuallist.rowHeight', 'imagovirtuallist.rowWidth'], function() {
+          var rowHeight, rowWidth;
+          if (!scope.imagovirtuallist.data) {
             return;
           }
-          if (typeof scope.rowWidth === 'string') {
-            rowWidth = parseInt(scope.rowWidth) / 100;
-            scope.rowWidth = scope.width * rowWidth;
+          if (angular.isString(scope.imagovirtuallist.rowWidth)) {
+            rowWidth = parseInt(scope.imagovirtuallist.rowWidth) / 100;
+            rowWidth = scope.width * rowWidth;
+          } else {
+            if (angular.isDefined(scope.imagovirtuallist.rowWidth)) {
+              rowWidth = scope.imagovirtuallist.rowWidth;
+            }
           }
+          if (angular.isDefined(scope.imagovirtuallist.rowHeight)) {
+            rowHeight = scope.imagovirtuallist.rowHeight;
+          }
+          return scope.init(rowWidth, rowHeight);
+        });
+        watcher = $rootScope.$on('imagovirtuallist:init', function() {
           return scope.init();
+        });
+        return scope.$on('$destroy', function() {
+          angular.element($window).off('scroll', scope.onScrollWindow);
+          return watcher();
         });
       }
     };
@@ -94,6 +135,6 @@ ImagoVirtualList = (function() {
 
 })();
 
-angular.module('imago').directive('imagoVirtualList', ['$timeout', ImagoVirtualList]);
+angular.module('imago').directive('imagoVirtualList', ['$window', '$rootScope', ImagoVirtualList]);
 
 angular.module("imago").run(["$templateCache", function($templateCache) {$templateCache.put("/imago/imago-virtual-list.html","<div ng-style=\"canvasHeight\" class=\"canvas\"></div>");}]);
--- src/imago-virtual-list/imago-virtual-list.directive.coffee
+++ src/imago-virtual-list/imago-virtual-list.directive.coffee
@@ -1,44 +1,65 @@
 class ImagoVirtualList extends Directive
 
-  constructor: ($timeout) ->
+  constructor: ($window, $rootScope) ->
 
     return {
 
-      scope:
+      scope: true
+      templateUrl: '/imago/imago-virtual-list.html'
+      transclude: true
+      controller: -> return
+      controllerAs: 'imagovirtuallist'
+      bindToController:
         data: '='
         rowHeight: '='
         rowWidth: '='
-      templateUrl: '/imago/imago-virtual-list.html'
-      transclude: true
 
       link: (scope, element, attrs, ctrl, transclude) ->
 
         transclude scope, (clone, scope) ->
           element.children().append clone
 
-        scope.width = element[0].clientWidth
         scope.scrollTop = 0
+        scope.width = element[0].clientWidth
         scope.visibleProvider = []
         scope.cellsPerPage = 0
         scope.numberOfCells = 0
         scope.canvasHeight = {}
 
-        scope.init = ->
-          scope.height = element[0].clientHeight
-          element[0].addEventListener 'scroll', scope.onScroll
+        scope.init = (rowWidth, rowHeight) ->
+          if not rowWidth or not rowHeight
+            testDiv = document.createElement 'div'
+            testDiv.className = attrs.classitem
+            testDiv.id = 'virtual-list-test-div'
+            element.append testDiv
+            rowWidth = rowWidth or testDiv.clientWidth
+            rowHeight = rowHeight or testDiv.clientHeight
+            angular.element(element[0].querySelector('#virtual-list-test-div')).remove()
+
+          scope.rowWidth = rowWidth
+          scope.rowHeight = rowHeight
+
+          scope.width = element[0].clientWidth unless scope.width
+          scope.height = element[0].clientHeight unless scope.height
+
+          if attrs.imagoVirtualListContainer
+            element[0].addEventListener 'scroll', scope.onScrollContainer
+          else
+            angular.element($window).on 'scroll', scope.onScrollWindow
           scope.itemsPerRow = Math.round(scope.width / scope.rowWidth)
+          scope.canvasHeight = height: Math.ceil((scope.imagovirtuallist.data.length * scope.rowHeight) / scope.itemsPerRow) + 'px'
           scope.cellsPerPage = Math.round(scope.height / scope.rowHeight) * scope.itemsPerRow
           scope.numberOfCells = 3 * scope.cellsPerPage
-          scope.canvasHeight = height: Math.ceil((scope.data.length * scope.rowHeight) / scope.itemsPerRow) + 'px'
+          scope.margin = 0
           scope.updateDisplayList()
 
         scope.updateDisplayList = ->
           firstCell = Math.max(Math.floor(scope.scrollTop / scope.rowHeight) - (Math.round(scope.height / scope.rowHeight)), 0)
           cellsToCreate = Math.min(firstCell + scope.numberOfCells, scope.numberOfCells)
-          oldVisible = angular.copy scope.visibleProvider
+          # oldVisible = angular.copy scope.visibleProvider
           data = firstCell * scope.itemsPerRow
-          scope.visibleProvider = scope.data.slice(data, data + cellsToCreate)
-          return if angular.equals(scope.visibleProvider, oldVisible)
+          scope.visibleProvider = scope.imagovirtuallist.data.slice(data, data + cellsToCreate)
+          # return if angular.equals(scope.visibleProvider, oldVisible)
           chunks = _.chunk(scope.visibleProvider, scope.itemsPerRow)
           i = 0
           while i < scope.visibleProvider.length
@@ -52,19 +73,37 @@ class ImagoVirtualList extends Directive
                   return idx
 
             idx = findIndex()
-            scope.visibleProvider[i].styles = 'transform': "translate3d(#{(scope.rowWidth * idx.inside) + 'px'}, #{(firstCell + idx.chunk) * scope.rowHeight + 'px'}, 0)"
+            scope.visibleProvider[i].styles = 'transform': "translate3d(#{(scope.rowWidth * idx.inside) + scope.margin + 'px'}, #{(firstCell + idx.chunk) * scope.rowHeight + 'px'}, 0)"
             i++
 
-        scope.onScroll = ->
+        scope.onScrollContainer = ->
           scope.scrollTop = element.prop('scrollTop')
           scope.updateDisplayList()
           scope.$apply()
 
-        scope.$watchGroup ['data', 'rowHeight', 'rowWidth'], ->
-          return if not scope.data or not scope.rowWidth or not scope.rowHeight
-          if typeof scope.rowWidth is 'string'
-            rowWidth = parseInt(scope.rowWidth) / 100
-            scope.rowWidth = scope.width * rowWidth
+        scope.onScrollWindow = ->
+          scope.scrollTop = $window.pageYOffset
+          scope.updateDisplayList()
+          scope.$apply()
+
+        scope.$watchGroup ['imagovirtuallist.data', 'imagovirtuallist.rowHeight', 'imagovirtuallist.rowWidth'], ->
+          return unless scope.imagovirtuallist.data
+
+          if angular.isString scope.imagovirtuallist.rowWidth
+            rowWidth = parseInt(scope.imagovirtuallist.rowWidth) / 100
+            rowWidth = scope.width * rowWidth
+          else
+            rowWidth = scope.imagovirtuallist.rowWidth if angular.isDefined scope.imagovirtuallist.rowWidth
+
+          rowHeight = scope.imagovirtuallist.rowHeight if angular.isDefined scope.imagovirtuallist.rowHeight
+
+          scope.init(rowWidth, rowHeight)
+
+        watcher = $rootScope.$on 'imagovirtuallist:init', ->
           scope.init()
+
+        scope.$on '$destroy', ->
+          angular.element($window).off 'scroll', scope.onScrollWindow
+          watcher()
 
     }
